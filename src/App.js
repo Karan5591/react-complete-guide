1874a4c7a265eaa87db3c77096813a3bbcf10bda
karan5591 <karansingh5591@gmail.com>
2023-06-06 13:17:39 +0530

    First Commit React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get Started you baklol</h2>
+      <ExpenseItem></ExpenseItem>
+      <p>Food Rs 10</p>
+      <p>Petrol Rs 100</p>
+      <p>Movies Rs 200</p>
+    </div>
+  );
+}
+
+export default App;
