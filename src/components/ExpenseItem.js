9ba663af0a0fe4053ca279d02961a1446c741f37
karan5591 <karansingh5591@gmail.com>
2023-06-07 11:23:26 +0530

    Styling and dynamic working code added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,21 @@
+import "./ExpenseItem.css"
+
 function ExpenseItem()
 {
+    const expenseDate= new Date(2023, 6, 7);
+    const expenseTitle='car Insurace';
+    const expenseAmount=294;
+    const LocationOfExpenditure="Grocery"
     return(
-        <h2>Expense item!</h2>
+        <div className="expense-item">
+        <div>{expenseDate.toISOString()}</div>
+       <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h2>{LocationOfExpenditure}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+        
+        </div>
+       </div>
     );
 }
 export default ExpenseItem;
