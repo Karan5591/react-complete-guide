81ac4fd06858f6171c9061fa3fb8509115d9982c
karan5591 <karansingh5591@gmail.com>
2023-06-15 12:04:12 +0530

    Creating Making Components Smaller and Readable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses=[
+    {
+      id: 'e1',
+      title: 'car Insurance',
+      amount: 294,
+      date: new Date(2023, 6,7),
+      LocationOfExpenditure: 'Delhi'
+    },
+    {
+      id: 'e2',
+      title: 'Electricity bill',
+      amount: 100,
+      date: new Date(2023, 6,7),
+      LocationOfExpenditure: "HP"
+    },
+    {
+      id: 'e3',
+      title: 'Food',
+      amount: 29,
+      date: new Date(2023, 6,7),
+      LocationOfExpenditure: 'UP'
+    }
+  ]
+
+let newArr=[];
+expenses.forEach((exp, index)=>{
+  newArr.push(
+  <div key={index}><ExpenseItem
+  title={exp.title}
+  amount={exp.amount}
+  date={exp.date}
+  LocationOfExpenditure={exp.LocationOfExpenditure}
+  ></ExpenseItem></div>)
+})
+  
   return (
     <div>
       <h2>Let's get Started!</h2>
-      <ExpenseItem></ExpenseItem>
       
-    </div>
+     <div>{newArr}</div>
+      
+      </div>
   );
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,15 @@
 import "./ExpenseItem.css"
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 
-function ExpenseItem()
+function ExpenseItem(props)
 {
-    const expenseDate= new Date(2023, 6, 7);
-    const expenseTitle='car Insurace';
-    const expenseAmount=294;
-    const LocationOfExpenditure="Grocery"
+    
     return(
         <div className="expense-item">
-        <div>{expenseDate.toISOString()}</div>
-       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <h2>{LocationOfExpenditure}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
-        
-        </div>
-       </div>
+        <ExpenseDate date={props.date}/>
+        <ExpenseDetails amount={props.amount} title={props.title} LocationOfExpenditure={props.LocationOfExpenditure}/>
+         </div>
     );
 }
 export default ExpenseItem;
